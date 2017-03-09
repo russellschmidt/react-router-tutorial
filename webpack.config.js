8cee6d0d5ab076c2f1f2fb4e0609643a2d28af7c
Russell Schmidt <russell.schmidt@me.com>
2017-03-09 15:06:48 -0800

    got a production server going with minified files"

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
+// webpack.config.js
+
+// must import
+var webpack = require('webpack')
+
 module.exports = {
   entry: './index.js',
 
   output: {
+    path: 'public',
     filename: 'bundle.js',
-    publicPath: ''
+    publicPath: '/'
   },
-
+  // plugings that optimize build for production with a clever ternary operator
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
   }
+
+
 }
